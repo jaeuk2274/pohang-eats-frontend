6aa0e7ccce49d28a48e0aa339680cb72f39e82b0
jaeuk2274 <jaeuk2274@gmail.com>
2021-01-12 22:10:48 +0900

    Apollo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
+import {client} from "./apollo";
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="bg-black"> 
       <h1 className="text-xl text-white ">hello tailwind</h1>
     </div>
+    </ApolloProvider>
   );
 }
 

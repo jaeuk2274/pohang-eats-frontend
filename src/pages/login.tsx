60caad0640b7318fe1aa737846b19f43acf04913
jaeuk2274 <jaeuk2274@gmail.com>
2021-01-15 11:33:48 +0900

    Login Mutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import { gql, useMutation } from "@apollo/client";
+import { ApolloError, gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
@@ -20,59 +20,75 @@ const LOGIN_MUTATION = gql`
 interface ILoginForm {
   email: string;
   password: string;
+  resultError?: string;
 }
 
 export const Login = () => {
-    const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
-    const [loginMutation, {data}] = useMutation<loginMutation, loginMutationVariables>(LOGIN_MUTATION);
-    const onSubmit = () => {
-      const { email, password } = getValues();
-      loginMutation({
-        variables: {
-          loginInput: {
-            email,
-            password,
-          },
+  const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+  const onCompleted = (data: loginMutation) => {
+    const {
+      login: { error, ok, token },
+    } = data;
+    if (ok) {
+      console.log(token);
+    }
+  };
+  const onError = (error: ApolloError) => {console.log('onError')} // return ok:false는 에러 아님.
+  const [loginMutation, { data: loginMutationResult }] = useMutation<loginMutation, loginMutationVariables>(LOGIN_MUTATION, {
+    onCompleted,
+    onError
+  });;
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        loginInput: {
+          email,
+          password,
         },
-      });
-    };
+      },
+    });
+  };
 
-    return (
-        <div className="h-screen flex items-center justify-center bg-gray-800">
-          <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-            <h3 className="text-2xl text-gray-800">Log In</h3>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="grid gap-3 mt-5 px-5"
-            >
-              <input
-                ref={register({ required: "Email is required" })}
-                name="email"
-                required
-                type="email"
-                placeholder="Email"
-                className="input"
-              />
-              {errors.email?.message && (
-                <FormError errorMessage={errors.email?.message} />
-              )}
-              <input
-                ref={register({ required: "Password is required" })}
-                required
-                name="password"
-                type="password"
-                placeholder="Password"
-                className="input"
-              />
-              {errors.password?.message && (
-                <FormError errorMessage={errors.password?.message} />
-              )}
-              {errors.password?.type === "minLength" && (
-                <FormError errorMessage="Password must be more than 10 chars." />
-              )}
-              <button className="mt-3 btn">Log In</button>
-            </form>
-          </div>
-        </div>
-      );
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+        <h3 className="text-2xl text-gray-800">Log In</h3>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
+          <input
+            ref={register({ required: "Email is required" })}
+            name="email"
+            required
+            type="email"
+            placeholder="Email"
+            className="input"
+          />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message} />
+          )}
+          <input
+            ref={register({ required: "Password is required" })}
+            required
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+          {errors.password?.type === "minLength" && (
+            <FormError errorMessage="Password must be more than 10 chars." />
+          )}
+          <button className="mt-3 btn">Log In</button>
+          {loginMutationResult?.login.error && (
+            <FormError errorMessage={loginMutationResult.login.error} />
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };

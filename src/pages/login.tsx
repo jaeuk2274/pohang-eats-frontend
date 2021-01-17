293175318de8cc8a61f21bba302f8ab951cabef8
jaeuk2274 <jaeuk2274@gmail.com>
2021-01-18 00:07:10 +0900

    Create Account

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,11 @@
 import { ApolloError, gql, useMutation } from "@apollo/client";
 import React from "react";
+import Helmet from 'react-helmet'
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { Button } from "../components/button";
 import { FormError } from "../components/form-error";
-import nuberLogo from "../images/logo.svg";
+import poberLogo from "../images/logo.svg";
 import {
   loginMutation,
   loginMutationVariables,
@@ -64,8 +65,11 @@ export const Login = () => {
 
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>Login | Pohang Eats</title>
+      </Helmet>
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
-        <img src={nuberLogo} className="w-52 mb-10" />
+        <img src={poberLogo} className="w-52 mb-10" />
         <h4 className="w-full font-medium text-left text-3xl mb-5">
           Welcome back
         </h4>
@@ -108,7 +112,7 @@ export const Login = () => {
           )}
         </form>
         <div>
-          New to Nuber?{" "}
+          New to Pober?{" "}
           <Link to="/create-account" className="text-lime-600 hover:underline">
             Create an Account
           </Link>

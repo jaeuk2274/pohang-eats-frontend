aee8728e1bcf8a166b3c9bc517a4b6306a57cfe0
jaeuk2274 <jaeuk2274@gmail.com>
2021-02-01 10:32:54 +0900

    FormError and Restaurant Tests

---
+++ src/components/__test__/restaurant.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Restaurant } from '../restaurant';
+
+describe("<Render />", () => {
+    it("renders OK with props", () => {
+        const restaurantProps = {
+            id: "1",
+            name: "name",
+            categoryName: "categoryName",
+            coverImg: "lala",
+            };
+
+        const { debug, getByText, container } = render(
+            <Router>
+                <Restaurant {...restaurantProps} />
+            </Router>
+        );
+        getByText(restaurantProps.name);
+        getByText(restaurantProps.categoryName);
+        expect(container.firstChild).toHaveAttribute(
+            "href", 
+            `/restaurants/${restaurantProps.id}`
+        )
+    })
+})

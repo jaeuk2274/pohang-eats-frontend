21e7578b9ea964e966d6ddc1df524bb366e39453
jaeuk2274 <jaeuk2274@gmail.com>
2021-02-01 09:36:21 +0900

    Test Setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { isLoggedInVar } from './apollo';
 import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
+// 미사용, /components/app 사용
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   console.log("isLoggedIn", isLoggedIn);

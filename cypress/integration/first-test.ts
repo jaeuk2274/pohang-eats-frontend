123fb34868a4506bcde4e6cd05dbbd1505d1c3a6
jaeuk2274 <jaeuk2274@gmail.com>
2021-02-02 17:53:33 +0900

    Cypress Setup

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,16 +1,20 @@
-describe("First Test", () => {
+describe("Log In", () => {
+    const user = cy;
     it("should see login page", () => {
-      cy.visit("/").title().should("eq", "Login | Pohang Eats");
+      user.visit("/").title().should("eq", "Login | Pohang Eats");
     });
 
-    it("can fill out the form", () => {
-      cy.visit("/")
-        .findByPlaceholderText(/email/i)
-        .type("jaeuk2274@gmail.com")
+    it("can see email / password validation errors", () => {
+      user.visit("/");
+      user.findByPlaceholderText(/email/i).type("bad@email");
+      user.findByRole("alert").should("have.text", "Please enter a valid email");
+      user.findByPlaceholderText(/email/i).clear();
+      user.findByRole("alert").should("have.text", "Email is required");
+      user.findByPlaceholderText(/email/i).type("bad@email.com");
+      user
         .findByPlaceholderText(/password/i)
-        .type("12345")
-        .findByRole("button")
-        .should("not.have.class", "pointer-events-none");
-      // to do (can log in)
+        .type("a")
+        .clear();
+      user.findByRole("alert").should("have.text", "Password is required");
     });
 });

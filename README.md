9f8dfa3e7a49388f1b7dbe6ad5aa3dd23cb43a54
jaeuk2274 <jaeuk2274@gmail.com>
2021-02-02 17:50:43 +0900

    Cypress Setup

--- README.md
+++ README.md
@@ -13,4 +13,9 @@
 - rimraf
     - win,moc,linux 크로스 플랫폼 명령어 지원 / rm rf
 
+- cypress 
+    - https://www.cypress.io/
+    - npm i cypress
+    - https://docs.cypress.io/guides/component-testing/introduction.html#Vue
+    - npm install --save-dev cypress cypress-react-unit-test
 
--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,16 @@
 describe("First Test", () => {
-    it("should go to homepage", () => {
-      cy.visit("http://localhost:3000")
-        .title()
-        .should("eq", "Login | Pohang Eats");
+    it("should see login page", () => {
+      cy.visit("/").title().should("eq", "Login | Pohang Eats");
     });
-  });
+
+    it("can fill out the form", () => {
+      cy.visit("/")
+        .findByPlaceholderText(/email/i)
+        .type("jaeuk2274@gmail.com")
+        .findByPlaceholderText(/password/i)
+        .type("12345")
+        .findByRole("button")
+        .should("not.have.class", "pointer-events-none");
+      // to do (can log in)
+    });
+});
